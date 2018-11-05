4cdc3a2446ff67a95db672c5bbee4a95d5043856
Alexnguyen17 <Alexnguyen170395@gmail.com>
2018-11-05 08:50:40 +0700

    hh

---
+++ MyListTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexnguyen
+ * Date: 05/11/2018
+ * Time: 08:48
+ */
+include 'MyList.php';
+$myList = new MyList();
+$myList->add(1);
+$myList->add(2);
+$myList->add(8);
+$myList->add(6);
+$myList->add(0);
+echo implode('-',$myList->toArray()) . '<br>';
+//$myList->remove(4);
+//$myList->addIndex(4,'a');
+//echo implode('-',$myList->toArray()) . '<br>';
+//$myList->sort();
+//echo implode('-',$myList->toArray()) ;
+echo $myList->contains(9);
